e92d1851326dd5cb86873302480fbdffc53ec926
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-10-19 21:41:06 -0600

    added restaurants model

--- restaurantchooser/restaurantapp/models.py
+++ restaurantchooser/restaurantapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Restaurants(models.Model):
+    name = models.CharField()
+    type1 = models.CharField()
+    type2 = models.CharField()
+    cost = models.CharField()
+    
